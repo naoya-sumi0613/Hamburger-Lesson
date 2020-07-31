8149395df44ec7564f66eb7c635cc13f27f38f5a
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-07-31 19:25:11 +0900

    add Hamburger script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(function() {
 
-	$('.menu-trigger').on('click', function() {
-		$(this).toggleClass('active');
-		$('#sp-menu').fadeToggle();
-		return false;
-	});
+	$('.menu-trigger').on('click', function(event) {
 
+    //.toggleClassは指定されているclass('active')をトグル処理する
+    //トグル処理とはclass属性がある場合はclassが削除されない場合はclassを追加する
+    $(this).toggleClass('active');
+
+    //.fadeToggleは要素のフェードイン・フェードアウトを切り替えるメソッド
+		$('#sp-menu').fadeToggle();
+		event.preventDefault();
+	});
 });
